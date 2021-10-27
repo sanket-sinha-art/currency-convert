82f18fe3c737d3e338cca0a96918ad5131caac9a
sanket099 <sinhasanket099@gmail.com>
2021-10-27 20:32:52 +0530

    api request

--- CurrencyConv/app/src/main/java/com/sanket/currencyconvertor/MainActivity.kt
+++ CurrencyConv/app/src/main/java/com/sanket/currencyconvertor/MainActivity.kt
@@ -21,6 +21,9 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    var baseCurrency = "EUR"
+    var convertedCurrency = "USD"
+    var rate = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +31,51 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+    }
+
+
+    private fun getApiResult() {
+        if (et_first_conversion != null && et_first_conversion.text.isNotEmpty() && et_first_conversion.text.isNotBlank()) {
+
+            var API =
+                "https://api.ratesapi.io/api/latest?base=$baseCurrency&symbols=$convertedCurrency"
+
+            if (baseCurrency == convertedCurrency) {
+                Toast.makeText(
+                    applicationContext,
+                    "Please pick a currency to convert",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+
+                GlobalScope.launch(Dispatchers.IO) {
+
+                    try {
+
+                        val apiResult = URL(API).readText()
+                        val jsonObject = JSONObject(apiResult)
+                        rate =
+                            jsonObject.getJSONObject("rates").getString(convertedCurrency)
+                                .toFloat()
+
+                        Log.d("Main", "$rate")
+                        Log.d("Main", apiResult)
+
+                        withContext(Dispatchers.Main) {
+                            val text =
+                                ((et_first_conversion.text.toString()
+                                    .toFloat()) * rate).toString()
+                            et_secondConversion?.setText(text)
+
+                        }
+
+                    } catch (e: Exception) {
+                        Log.e("Main", "$e")
+                    }
+                }
+            }
+        }
     }
 
 
